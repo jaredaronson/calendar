76bb3616d4f95413245a8aa91ca8878d7c7981b3
jaredaronson <jared@madhousetees.com>
2017-01-05 17:35:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calendar</title>
+    <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+</head>
+<body>
+
+<?php
+
+    date_default_timezone_set("America/New_York");
+
+    $currentDayOfTheMonth = date(j);
+    echo $currentDayOfTheMonth;
+    $currentYear = date(Y);
+    $currentMonth = date(F);
+
+    $time = strtotime("1 " .$currentMonth." ".$currentYear);
+
+?>
+
+</body>
+</html>
